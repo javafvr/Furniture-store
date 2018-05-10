32948f44ef0032f41db12545b05b4ea8692987b7
Dmitrii <javafvr@yahoo.com>
2018-05-10 17:03:50 +0500

    add navigation

---
+++ docs/js/main.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+	var pull=$("#navigation-toggle"),
+		menu = $(".navigation ul");
+
+	$(pull).on('click', function(e){
+		
+		e.preventDefault();
+
+		$(menu).slideToggle();
+
+		//Добавим модификатор
+		$(this).toggleClass("navigation__toggle-button--active");
+	});
+
+	// Owl-carousel version 2
+	$("#top-slider").owlCarousel({
+		items:1,
+		rewind:true,
+		// nav:false,
+		dots:true,
+		navText:["",""],
+		responsive : {
+		// breakpoint from 0 up
+			0 : {
+				nav:false,
+				dots:true,
+
+			},
+		// breakpoint from 480 up
+			320 : {
+				nav:false,
+				dots:true,
+			},
+		// breakpoint from 768 up
+			768 : {
+				dots:true,
+
+			},
+		// breakpoint from 1200 up
+			1200 : {
+				dots:true,
+
+			}
+		}
+	});
+
+	// var w = $(window).width();
+	// if(w < 992){
+	// 	console.log("Клик по ссылке " + w);
+	// 	$('nav.navigation a').on("click", function(){
+	// 		menu.slideToggle();
+	// 	});
+	// }
+
+
+	// Скрываем меню при клике на него на смартфоне и планцете
+	// По клику на ссылку в меню запускаем ф-ю fnstart();
+	$('nav.navigation a').on("click", function(){
+		fnstart();
+	});
+
+	// В ф-ии fnstart(); проверяем - если меню открыто (проверяем по наличию класса --active у кнопки pull)
+	// тогда убираем класс модификатор --active у кнопки pull
+	// и сворачиваем/скрываем меню 
+	function fnstart(){	
+		if ( $("#navigation-toggle").hasClass("navigation__toggle-button--active")  ) {
+			pull.toggleClass('navigation__toggle-button--active');
+			menu.slideToggle();
+		}
+	};
+
+
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 768 && menu.is(':hidden')){
+			menu.removeAttr('style');
+		}
+
+	});
+
+	//slide2id - плавная прокрутка по ссылкам внутри страницы
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+		highlightSelector:"nav a"
+	});
+
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,58 @@
 var gulp = require('gulp'),
-	browserSync = require('browser-sync').create();
+	browserSync = require('browser-sync').create(),
+	notify = require('gulp-notify'),
+	plumber = require('gulp-plumber'),
+      less = require("gulp-less"),
+	pug = require('gulp-pug');
 
-gulp.task('default', function() {
+gulp.task('server', function() {
 	browserSync.init({
 		server: { baseDir: './docs'}
 	});
-	gulp.watch('./docs/**/*.html').on('change', browserSync.reload);
+	gulp.watch('docs/**/*.html').on('change', browserSync.reload);
+	gulp.watch('docs/js/**/*.js').on('change', browserSync.reload);
+	gulp.watch('less/**/*.less', ['less'] );
+	gulp.watch('pug/**/*.pug', ['pug'] );
 });
 
+gulp.task('pug', () => {
+    return gulp.src('./pug/*.pug')
+      .pipe(plumber({
+            errorHandler: notify.onError(function(err){
+                  return {
+                        title: 'Pug',
+                        message: err.message
+                  }
+            })
+      }))
+      .pipe(pug({
+            pretty: true
+      }))
+      .pipe(gulp.dest('./docs'))
+      .pipe(browserSync.stream());
+});
 
+gulp.task('less', () => {
+    return gulp.src('./less/**/*.less')
+      .pipe(plumber({
+            errorHandler: notify.onError(function(err){
+                  return {
+                        title: 'Styles',
+                        message: err.message
+                  }
+            })
+      }))
+      .pipe(less())
+      .pipe(gulp.dest('docs/css'))
+      .pipe(browserSync.stream());
+});
 
-
-
-// var	gulp = 		require('gulp'),
-// 		browserSync = require('browser-sync').create(),
-// 		less = 		require("gulp-less"),
-//       	notify = 	require('gulp-notify'),
-//       	plumber = 	require('gulp-plumber'),
-//       	pug = 		require('gulp-pug');
-
-// gulp.task("server", ['less','pug'],function () {
-// 	browserSync.init({
-// 		server: { baseDir: './docs' }
-// 	});
-//       gulp.watch('**/*.html').on('change', browserSync.reload);
-// 	gulp.watch('docs/js/**/*.js').on('change', browserSync.reload);
-//       gulp.watch('less/**/*.less', ['less'] );
-//       gulp.watch('pug/**/*.pug', ['pug'] );
+// // Uglify Task
+// gulp.task('uglify', function(){
+//   gulp.src('docs/js/*.js')
+//     .pipe(plumber()) // plumber
+//     //.pipe(uglify())
+//     .pipe(gulp.dest('docs/js'))
 // });
 
-
-
-// gulp.task('pug', () => {
-//     return gulp.src('./pug/*.pug')
-//       .pipe(plumber({
-//             errorHandler: notify.onError(function(err){
-//                   return {
-//                         title: 'Pug',
-//                         message: err.message
-//                   }
-//             })
-//       }))
-//       .pipe(pug({
-//             pretty: true
-//       }))
-//       .pipe(gulp.dest('./docs'))
-//       .pipe(browserSync.stream());
-// });
-
-// gulp.task('less', () => {
-//     return gulp.src('./less/**/*.less')
-//       .pipe(plumber({
-//             errorHandler: notify.onError(function(err){
-//                   return {
-//                         title: 'Styles',
-//                         message: err.message
-//                   }
-//             })
-//       }))
-//       .pipe(less())
-//       .pipe(gulp.dest('docs/css'))
-//       .pipe(browserSync.stream());
-// });
-
-// // // Uglify Task
-// // gulp.task('uglify', function(){
-// //   gulp.src('docs/js/*.js')
-// //     .pipe(plumber()) // plumber
-// //     //.pipe(uglify())
-// //     .pipe(gulp.dest('docs/js'))
-// // });
-
-// gulp.task('default', ["server"]);
+gulp.task('default', ["server"]);
