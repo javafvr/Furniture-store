0b58be58d6263abbb85ff29936bb1aa3c4d40b8a
Dmitrii <javafvr@yahoo.com>
2018-08-12 11:53:58 +0500

    fix bugs

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,52 +1,46 @@
 $(document).ready(function(){
 	var pull=$("#navigation-toggle"),
 		menu = $("#nav_mobile-list");
+		logo = $('.logo__icon');
 
 	$(pull).on('click', function(e){
 		
 		e.preventDefault();
 
 		$(menu).slideToggle();
+		
+		rotateIcon();
 
-		//Добавим модификатор
-		$(this).toggleClass("navigation__toggle-button--active");
-	});
-	
-	// Скрываем меню при клике на него на смартфоне и планцете
-	// По клику на ссылку в меню запускаем ф-ю fnstart();
-	$('nav.navigation a').on("click", function(){
-		fnstart();
 	});
 
-	// В ф-ии fnstart(); проверяем - если меню открыто (проверяем по наличию класса --active у кнопки pull)
-	// тогда убираем класс модификатор --active у кнопки pull
-	// и сворачиваем/скрываем меню 
-	function fnstart(){	
-		if ( $("#navigation-toggle").hasClass("navigation__toggle-button--active")  ) {
-			pull.toggleClass('navigation__toggle-button--active');
-			menu.slideToggle();
-		}
-	};
+	// Вращает логотип на 45 градусов в мобильной версии
+	function rotateIcon() {
+		var w = $(window).width();
+		if(w < 768){$('.logo__icon').toggleClass("rotate");}
+	}
 
 
 	$(window).resize(function(){
 		var w = $(window).width();
-		if(w > 768 && menu.is(':hidden')){
-			menu.removeAttr('style');
+		//Поворачиваем назад иконку если меню открыто
+		// и не трогаем если меню скрыто
+		if(w > 768){
+			$(logo).removeClass("rotate");
+		} else if (w < 768 && menu.is(':visible')){
+			$(logo).addClass("rotate");	
 		}
 
+		if(w > 768 && menu.is(':hidden')){
+			menu.removeAttr('style');
+			$('.logo__icon').removeClass("rotate");
+		}
 	});
-
-
-
-
-
 
 	// Owl-carousel version 2
 	$("#top-slider").owlCarousel({
 		items:1,
 		rewind:true,
-		autoplay:true,
+		autoplay:false,
 		smartSpeed: 2000,
 		
 		fluidSpeed: 2000,
@@ -114,19 +108,8 @@ $(document).ready(function(){
 		}
 	});
 
-	// var w = $(window).width();
-	// if(w < 992){
-	// 	console.log("Клик по ссылке " + w);
-	// 	$('nav.navigation a').on("click", function(){
-	// 		menu.slideToggle();
-	// 	});
-	// }
-
-
-
 	// mixItUp2 фильтр работ в портфолио
 	$('#featured-products').mixItUp();
-
 
 	//slide2id - плавная прокрутка по ссылкам внутри страницы
 	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
